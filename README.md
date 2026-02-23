58cc7cdc9effa77f777605f9ee6da8f5cb9ee529
Jose Ocampo <ocampojose305@gmail.com>
2026-02-23 12:59:30 -0500

    push package changes to github

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# slider-js
+# @jocampo3/slider
+
+A lightweight, minimal carousel built with CSS and vanilla JavaScript.
+
+## Why?
+
+Tired of bulky external NPM packages? This package delivers essential carousel functionality with minimal JavaScript, keeping your projects fast and clean.
+
+## Install
+
+```bash
+npm install @jocampo3/slider
+```
+
+## Usage
+
+### Single Slider
+
+```javascript
+import Slider from '@jocampo3/slider';
+import '@jocampo3/slider/styles.css';
+
+const interval = 3500; // update to your interval value
+const slider = new Slider('#my-slider');
+```
+
+### Multiple Sliders
+
+```javascript
+import Slider from '@jocampo3/slider';
+import '@jocampo3/slider/styles.css';
+
+const interval = 3500; // update to your interval value
+const slides = document.querySelectorAll('.slideshow');
+
+slides.forEach(s => new Slider(s, interval));
+```
+
+## Features
+
+- Lightweight (~1KB gzipped)
+- Zero dependencies
+- CSS-driven animations
+- Fully customizable via CSS variables
+
+## License
+
+ISC
---
+++ index.js
@@ -0,0 +1,51 @@
+export default class Slider {
+  constructor(slider, interval) {
+    this.slider = slider;
+    this.slides = this.slider.children;
+    this.index = 0;
+    this.interval = interval;
+    this.timer = null;
+    this.restartTimer();
+    this.addListeners();
+  }
+
+  slideWidth() {
+    return this.slider.clientWidth;
+  }
+
+  restartTimer() {
+    clearInterval(this.timer);
+    this.timer = setInterval(() => this.next(), this.interval);
+  }
+
+  goTo(i) {
+    this.index = (i + this.slides.length) % this.slides.length;
+    this.slider.scrollTo({ left: this.index * this.slideWidth(), behavior: 'smooth' });
+    this.restartTimer();
+  }
+
+  next() {
+    this.goTo(this.index + 1);
+  }
+
+  getCurrentIndex() {
+    return Math.round(this.slider.scrollLeft / this.slideWidth());
+  }
+
+  addListeners() {
+    this.slider.addEventListener('scroll', () => {
+      this.index = this.getCurrentIndex();
+    })
+    this.slider.addEventListener('mouseenter', () => {
+      clearInterval(this.timer);
+    });
+
+    this.slider.addEventListener('mouseleave', () => {
+      this.restartTimer();
+    });
+
+    window.addEventListener('resize', () => {
+      this.goTo(this.index);
+    });
+  }
+}
